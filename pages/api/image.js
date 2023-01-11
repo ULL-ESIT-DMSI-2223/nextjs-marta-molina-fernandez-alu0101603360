6b04727d71e8e7dfc560f8464770bbb5d3f21019
Marta Molina Fernández <83011996+MartaMolina01@users.noreply.github.com>
2023-01-11 09:40:18 +0000

    image gen

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,20 +1,21 @@
-import { Configuration, OpenAIApi } from "openai"; //Esto se ejecuta en el servidor por estar en /pages/api esto le llega al servidor con la solicitud del cliente, elabormaos parte de la respuesta el resto completado por servidor y se devuelve al cliente
+import { Configuration, OpenAIApi } from "openai"; 
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY, //Nos devuleve el valor de la variable de entorno OPEN_API_KEY
+  apiKey: process.env.OPENAI_API_KEY, 
 });
 const openai = new OpenAIApi(configuration);
 
-export default async function (req, res) { //No se saben cuando se van a llamar así que async y por ello desps hay q poner await
-  const completion = await openai.createCompletion({
-    model: "text-davinci-002", //identificador del modelo que vas a usar de OpenAI, puedes pedir una lista
+export default async function (req, res) { 
+  const image = await openai.create({
     prompt: generatePrompt(req.body.animal),
-    temperature: 0.6, //nivel de riesgos, cuanto más alto más creativo
+    n: 1,
+    size: "1024x1024",
+    temperature: 0.6,
   });
-  res.status(200).json({ result: completion.data.choices[0].text });
+  res.status(200).json({ result: image.data.choices[0].url });
 }
 
-function generatePrompt(animal) {
+/* function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
   console.log(capitalizedAnimal);
@@ -27,4 +28,4 @@ Animal: ${capitalizedAnimal}
 Names:`;
   console.log(suggestion);
   return suggestion;
-}
+} */
