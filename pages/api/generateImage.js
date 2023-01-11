9d804fed8e5165a4e391dc45c676a3d239baf90a
Marta Molina Fernández <83011996+MartaMolina01@users.noreply.github.com>
2023-01-11 18:49:20 +0000

    x4

--- pages/api/generateImage.js
+++ pages/api/generateImage.js
@@ -1,16 +1,19 @@
-import { Configuration, OpenAIApi } from "openai"; 
+
+
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY, 
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-export default async function (req, res) { 
+export default async function (req, res) {
   console.log(req.body);
-  const image = await openai.createImage({
-    prompt: req.body.prompt,
-    n: 1,
-    size: req.body.size,
-  });
-  res.status(200).json({ result: image.data.data[0].url });
+  const response = await openai.createImage({
+      prompt: req.body.prompt,
+      n: 1,
+      size: req.body.size,
+    });
+    const image_url = response.data.data[0].url;
+    res.status(200).json({ result: image_url });
 }
