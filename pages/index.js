bf0bfe694738846965dac8a7b0ced8fbfb1c5098
Marta Molina Fernández <83011996+MartaMolina01@users.noreply.github.com>
2023-01-11 21:03:44 +0000

    optimización

--- pages/index.js
+++ pages/index.js
@@ -26,11 +26,11 @@ export default function Home() {
     <div>
       <Head>
         <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/animal_icon.png" />
+        <link rel="icon" href="/photo_icon.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/animal_icon.png" className={styles.icon} />
+        <img src="/photo_icon.png" className={styles.icon} />
         <h3>Photo generator</h3>
         <form onSubmit={onSubmit}>
           <input
@@ -49,3 +49,46 @@ export default function Home() {
   );
 
 }
+
+/* export default function Home() {
+  const [animalInput, setAnimalInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ animal: animalInput }),
+    });
+    const data = await response.json();
+    setResult(data.result);
+    setAnimalInput("");
+  }
+  return (
+    <div>
+      <Head>
+        <title>OpenAI Quickstart</title>
+        <link rel="icon" href="/dog.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/dog.png" className={styles.icon} />
+        <h3>Name my pet</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="animal"
+            placeholder="Enter an animal"
+            value={animalInput}
+            onChange={(e) => setAnimalInput(e.target.value)}
+          />
+          <input type="submit" value="Generate names" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+} */
